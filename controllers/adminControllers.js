e104ad8b9dc950f62a2910c9b47348c140d80fa9
preethamkh <blighter.soul@gmail.com>
2016-09-19 18:19:03 +1000

    Reading Orders off an AJAX request and displays order details

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -2,7 +2,8 @@
  * Created by Preetham on 16/09/2016.
  */
 angular.module("sportsStoreAdmin")
-        .constant("authUrl", "http://localhost:5500/users/login")
+    .constant("authUrl", "http://localhost:5500/users/login")
+    .constant("ordersUrl", "http://localhost:5500/orders")
     .controller("authCtrl", function ($scope, $http, $location, authUrl) {
         $scope.authenticate = function (user, pass) {
             $http.post(authUrl, {
@@ -15,5 +16,40 @@ angular.module("sportsStoreAdmin")
             }).error(function (error) {
                 $scope.authenticationError = error;
             });
+        }
+    })
+    .controller("mainCtrl", function ($scope) {
+        $scope.screens = ["Products", "Orders"];
+        $scope.current = $scope.screens[0];
+
+        $scope.setScreen = function (index) {
+            $scope.current = $scope.screens[index];
+        }
+
+        $scope.getScreen = function () {
+            return $scope.current == "Products" ? "/views/adminProducts.html" : "/views/adminOrders.html";
+        }
+    })
+    .controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+        $http.get(ordersUrl, {withCredentials: true})
+            .success(function (data) {
+                $scope.orders = data;
+            })
+            .error(function (error) {
+                $scope.error = error;
+            });
+
+        $scope.selectedOrder;
+
+        $scope.selectOrder = function (order) {
+            $scope.selectedOrder = order;
+        };
+
+        $scope.calcTotal = function (order) {
+            var total = 0;
+            for (var i = 0; i < order.products.length; i++) {
+                total += order.products[i].count * order.products[i].price;
+            }
+            return total;
         }
     });
